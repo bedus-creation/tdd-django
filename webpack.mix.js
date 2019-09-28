b7c0ce53b3feb2d85f2638dd5765b436723bcea0
Bedus Creation <tmgbedu@gmail.com>
2019-09-29 01:25:09 +0545

    Vue+ Tailwind + Dynamic import component setup :rocket:

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,24 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-purgecss');
+
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': __dirname + '/resources/js'
+        },
+    },
+    output: {
+        chunkFilename: 'js/chunks/[name].js?id=[hash]',
+    },
+});
+
 
 mix.js('resources/js/app.js', 'static/js')
-    .sass('resources/sass/app.scss', 'static/css');
+    .extract(['vue']);
+mix.sass('resources/sass/app.scss', 'static/css');
+mix.postCss('resources/sass/tailwind.css', 'static/css', [
+    require('tailwindcss'),
+]).purgeCss();
